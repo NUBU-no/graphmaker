d73d421030ed223fb7d8ce0bcb1dd4340ad0557d
asteennilsen@gmail.com <asteennilsen@gmail.com>
2020-10-01 22:30:08 +0200

    Finnish for tonight, done bootsrap etc

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from wtforms.fields.core import SelectField
 from wtforms.widgets.core import TextArea
 from graphmaker.maker import make_graph, make_file
 from flask import Flask, json, request, jsonify, render_template, send_file
-from wtforms import Form, BooleanField, StringField, validators, IntegerField, widgets
+from wtforms import Form, BooleanField, StringField, validators, IntegerField, widgets, FormField
 import pandas as pd
 from io import BytesIO, StringIO
 app = Flask(__name__)
@@ -12,7 +13,7 @@ class Graph_form(Form):
                          validators.Length(min=1, max=25)])
     x_axis = StringField('X axis key', default='key', validators=[
                          validators.Length(min=1, max=25)])
-    x_axis = StringField('Y axis key', default='value', validators=[
+    y_axis = StringField('Y axis key', default='value', validators=[
                          validators.Length(min=1, max=25)])
     hue_key = StringField('Hue (farge) key', default='time', validators=[
                           validators.Length(min=1, max=25)])
@@ -35,6 +36,15 @@ class Graph_form(Form):
 5	94	t2	emo
 6	52	t2	hyp''')
 
+class Rcads_form(Form):
+    q1 = IntegerField('rcads spm 1', validators=[validators.NumberRange(min=0,max=5)])
+    q2 = IntegerField('rcads spm 2', validators=[validators.NumberRange(min=0,max=5)])
+
+
+class Questionnaire_form(Form):
+    questionnaire = SelectField('sdq, Eiberg or RCADS', choices=['SDQ','RCADS','Eiberg'])
+    #inline_form = FormField()
+
 
 @ app.route('/')
 def hello_world():
@@ -49,7 +59,7 @@ def json_to_svg(df, settings=None):
     return fig_file
 
 
-@ app.route('/excelpaste',  methods=['GET', 'POST'])
+@ app.route('/simple',  methods=['GET', 'POST'])
 def excelpaste():
     form=Graph_form(request.form)
 
@@ -61,9 +71,23 @@ def excelpaste():
         df=pd.read_csv(string_IO, sep='\t')
         svg=json_to_svg(df, settings)
         return svg.getvalue()
-    else:
+    elif request.method == 'GET':
         return render_template('test.html', form=form)
+    else:
+        return render_template('test.html', form=form), 400
 
+
+@ app.route('/rcads', methods=['GET', 'POST'])
+def single_items():
+    form = Rcads_form(request.form)
+    if request.method == 'POST' and form.validate():
+        return 'You posted'
+    else:
+        return render_template('all_fields.html', form=form)
+
+@app.route('/questionsexample')
+def example():
+    return render_template('example.html')
 
 if __name__ == '__main__':
     app.run()
